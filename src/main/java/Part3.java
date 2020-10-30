2d5ea7e2532138c8026c067aa1a49853957cbb80
Oleg Kaplun <oleg.kaplun@booking.com>
2020-10-30 16:03:38 +0200

    part 3 - complete solution

---
+++ src/main/java/Part3.java
@@ -0,0 +1,55 @@
+import operations.OperationChain;
+
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Part3 {
+
+    public static void main(String[] args) {
+
+        var inputFileNames = List.of("0.csv", "1.csv", "2.csv", "3.csv", "4.csv", "5.csv");
+        var outputFilePrefix = "result";
+
+        List<Stream<String>> inputStreams = new LinkedList<>();
+        for (var fileName : inputFileNames) {
+            try {
+                Stream<String> stream = Files.lines(Paths.get(Part3.class.getResource(fileName).toURI()));
+                inputStreams.add(stream);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        var classAverage = OperationChain
+            .create(inputStreams, OperationChain.ChainMode.FOR_EACH)
+            .pluck(10)
+            .avg()
+            .streams();
+
+        var maxAverage = OperationChain
+            .create(classAverage, OperationChain.ChainMode.FLATTEN)
+            .max();
+
+        int i = 0;
+        for (var stream : maxAverage.streams()) {
+            try (OutputStream os = new FileOutputStream(String.format("%s%d.csv", outputFilePrefix, i))) {
+                String res = stream.collect(Collectors.joining("\n"));
+                os.write(res.getBytes());
+
+            }  catch (Exception e) {
+                e.printStackTrace();
+            }
+            i++;
+        }
+
+        for (var inputStream : inputStreams) {
+            inputStream.close();
+        }
+    }
+}
